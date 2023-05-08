d9a8f03a56e536827c1545a96a868d093fce0019
ellenwei <ellenwei@PersonalMicrosoftSoftware.ucla.edu>
2023-05-08 01:16:18 -0700

    Updated landing page may 8

---
+++ email.js
@@ -0,0 +1,7 @@
+const form_button = document.getElementById("form-btn");
+
+form_button.addEventListener("click", handle_form);
+
+function handle_form() {
+  window.open("https://2lla1pzpmkk.typeform.com/to/FNPEuyZe");
+}
